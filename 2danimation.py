198e4f3865558d4e2171e13efc9511cce10b8f41
xli2522 <50414011+xli2522@users.noreply.github.com>
2020-11-13 17:09:18 -0500

    Updated Animation .gif

---
+++ 2danimation.py
@@ -0,0 +1,74 @@
+# Reference: https://www.codeproject.com/Articles/1087025/Using-Python-to-Solve-Computational-Physics-Proble by Garbel Nervadof
+# Simple Numerical Laplace Equation Solution using Finite Difference Method
+import numpy as np
+import matplotlib.pyplot as plt
+from celluloid import Camera
+
+# Set maximum iteration
+maxIter = 1000
+
+# Set Dimension and delta
+lenX = lenY = 100 #we set it rectangular
+delta = 1
+
+# Boundary condition
+ytop = []
+ybottom = []
+xright = []
+xleft = []
+for i in range(lenX):
+    ybottom.append(np.sin(2*np.pi*i/100))
+    ytop.append(np.cos(np.pi*i/200))
+    xright.append([0])
+    xleft.append([i/100])
+
+# Initial guess of interior grid
+Tguess = 1
+
+# Set colour interpolation and colour map
+fig = plt.figure()
+colorinterpolation = 50
+colourMap = plt.cm.jet #you can try: colourMap = plt.cm.coolwarm
+
+# Set meshgrid
+X, Y = np.meshgrid(np.arange(0, lenX), np.arange(0, lenY))
+
+# Set array size and set the interior value with Tguess
+T = np.empty((lenX, lenY))
+T.fill(Tguess)
+
+# Set Boundary condition
+T[(lenY-1):, :] = np.copy(ytop)
+T[:1, :] = np.copy(ybottom)
+T[:, (lenX-1):] = np.copy(xright)
+T[:, :1] = np.copy(xleft)
+
+# Camera
+camera = Camera(fig)
+
+# Iteration
+count = 0
+for iteration in range(0, maxIter):
+    for i in range(1, lenX-1, delta):
+        for j in range(1, lenY-1, delta):
+            pre = T[i, j]
+            T[i, j] = 0.25 * (T[i+1][j] + T[i-1][j] + T[i][j+1] + T[i][j-1])
+            count+=1
+            #if abs(pre-T[i, j])/pre <= 0.000001:
+                #break
+     # Configure the contour
+    plt.title("Solution through Relaxation")
+    plt.contourf(X, Y, T, colorinterpolation, cmap=colourMap)
+
+    # Snapshot
+    camera.snap()    
+
+# Show the result in the plot window
+plt.show()
+
+# Save Animation
+animation = camera.animate()
+animation.save('solution.gif')
+
+print(str(count))
+
